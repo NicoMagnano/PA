7ff1d54494ac274baf6cae540c238cb060a3a161
NicoMagnano <nicomagnano12@gmail.com>
2024-09-30 22:17:27 -0300

    restauracion de commit sin dto

--- pa/src/main/java/com/example/pa/controller/DTO/CategoriaDTO/EliminarCategoriaDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.pa.controller.DTO.CategoriaDTO;
-
-import jakarta.validation.constraints.NotNull;
-
-public class EliminarCategoriaDto {
-    
-    @NotNull(message = "El ID no puede ser nulo")
-    private Long id;
-}
