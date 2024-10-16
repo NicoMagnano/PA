159b57a72cdcc9cd491ad8f853786a2ea4a81b66
fernando monti <fer.monti@hotmail.com.ar>
2024-10-16 02:16:22 -0300

    keep stashed changes

---
+++ pa/target/generated-sources/annotations/com/example/pa/controller/Mapper/MarcaMapperImpl.java
@@ -0,0 +1,57 @@
+package com.example.pa.controller.Mapper;
+
+import com.example.pa.controller.DTO.MarcaDTO.MarcaDTO;
+import com.example.pa.model.Marca;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-10-16T02:02:56-0300",
+    comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.40.0.v20240919-1711, environment: Java 17.0.12 (Eclipse Adoptium)"
+)
+@Component
+public class MarcaMapperImpl implements MarcaMapper {
+
+    @Override
+    public MarcaDTO toDTO(Marca marca) {
+        if ( marca == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String nombre = null;
+        String descripcion = null;
+        boolean activo = false;
+
+        id = marca.getId();
+        nombre = marca.getNombre();
+        descripcion = marca.getDescripcion();
+        activo = marca.isActivo();
+
+        MarcaDTO marcaDTO = new MarcaDTO( id, nombre, descripcion, activo );
+
+        return marcaDTO;
+    }
+
+    @Override
+    public Marca toEntity(MarcaDTO marcaDTO) {
+        if ( marcaDTO == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String nombre = null;
+        String descripcion = null;
+
+        id = marcaDTO.getId();
+        nombre = marcaDTO.getNombre();
+        descripcion = marcaDTO.getDescripcion();
+
+        boolean activo = false;
+
+        Marca marca = new Marca( id, nombre, descripcion, activo );
+
+        return marca;
+    }
+}
